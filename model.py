32f2ae0e4c7cd2e9d3ed068279f94396388dc668
Rajat U Krishna <rajatkrishna99@gmail.com>
2020-04-27 17:09:30 +0530

    modified alexnet, reduced learning rate for higher no of epochs

--- model.py
+++ model.py
@@ -8,44 +8,68 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        #1, 224, 224
-        self.conv1 = nn.Conv2d(1, 32, 4)
-        #32, 221, 221
-        self.pool1 = nn.MaxPool2d(2, 2)
-        #32, 110, 110
-        self.drop1 = nn.Dropout(p = 0.1)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        #64, 108, 108
-        self.pool2 = nn.MaxPool2d(2, 2)
-        #64, 54, 54
-        self.drop2 = nn.Dropout(p = 0.2)
-        self.conv3 = nn.Conv2d(64, 128, 2)
-        #128, 53, 53
-        self.pool3 = nn.MaxPool2d(2, 2)
-        #128, 26, 26
-        self.drop3 = nn.Dropout(p = 0.3)
-        self.conv4 = nn.Conv2d(128, 256, 1)
-        #256, 26, 26
-        self.pool4 = nn.MaxPool2d(2, 2)
-        #256, 13, 13
-        self.drop4 = nn.Dropout(p = 0.4)
-        self.fc1 = nn.Linear(256 * 13 * 13, 5000)
-        self.drop5 = nn.Dropout(p = 0.5)
-        self.fc2 = nn.Linear(5000, 1000)
-        self.drop6 = nn.Dropout(p = 0.6)
-        self.output = nn.Linear(1000, 136)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=96, kernel_size=(4, 4), stride=4, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=96, out_channels=256, kernel_size=(5, 5), stride=1, padding=2)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=384, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=384, out_channels=384, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3, 3), stride=1, padding=1)
+
+        ## Max-Pool layer
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
+
+        ## Linear layers
+        self.fc1 = nn.Linear(in_features=9216, out_features=4096)
+        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+        self.fc3 = nn.Linear(in_features=4096, out_features=136)
+
+        ## Dropout
+        self.dropout2 = nn.Dropout(p=0.2)
+        self.dropout4 = nn.Dropout(p=0.4)
+        self.dropout6 = nn.Dropout(p=0.6)
+
+        # Batch Normalization
+        self.bn1 = nn.BatchNorm2d(num_features=96, eps=1e-05)
+        self.bn2 = nn.BatchNorm2d(num_features=256, eps=1e-05)
+        self.bn3 = nn.BatchNorm2d(num_features=384, eps=1e-05)
+        self.bn4 = nn.BatchNorm2d(num_features=384, eps=1e-05)
+        self.bn5 = nn.BatchNorm2d(num_features=256, eps=1e-05)
+        self.bn6 = nn.BatchNorm1d(num_features=4096, eps=1e-05)
+        self.bn7 = nn.BatchNorm1d(num_features=4096, eps=1e-05)
 
 
     def forward(self, x):
-        x = self.drop1(self.pool1(F.elu(self.conv1(x))))
-        x = self.drop2(self.pool2(F.elu(self.conv2(x))))
-        x = self.drop3(self.pool3(F.elu(self.conv3(x))))
-        x = self.drop4(self.pool4(F.elu(self.conv4(x))))
+        x = F.elu(self.conv1(x))
+        x = self.pool(x)
+        x = self.dropout2(x)
 
+        x = F.elu(self.conv2(x))
+        x = self.bn2(x)
+        x = self.pool(x)
+
+        x = F.elu(self.conv3(x))
+        x = self.bn3(x)
+        x = self.dropout4(x)
+
+        x = F.elu(self.conv4(x))
+        x = self.bn4(x)
+        x = self.dropout4(x)
+
+        x = F.elu(self.conv5(x))
+        x = self.bn5(x)
+        x = self.pool(x)
+
+        ## Flatten
         x = x.view(x.size(0), -1)
-        x = self.drop5(F.elu(self.fc1(x)))
-        x = self.drop6(F.relu(self.fc2(x)))
-        x = self.output(x)
 
-        
+        ## Fully connected layers
+        x = F.elu(self.fc1(x))
+        x = self.bn6(x)
+        x = self.dropout6(x)
+
+        x = F.elu(self.fc2(x))
+        x = self.bn6(x)
+        x = self.dropout6(x)
+
+        x = self.fc3(x)
+
         return x
---
+++ train.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model import Net
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from preprocess import FacialDataset
+from preprocess import Rescale, RandomCrop, Normalize, ToTensor
+import torch.optim as optim
+
+data_transform = transforms.Compose([Rescale((227, 227)), Normalize(), ToTensor()])
+net = Net()
+transformed_dataset = FacialDataset(csv_file='/data/training_frames_keypoints.csv', root_dir='/data/training/', transform=data_transform)
+test_dataset = FacialDataset(csv_file='/data/test_frames_keypoints.csv', root_dir='/data/test/', transform=data_transform)
+
+print('Number of images: ', len(transformed_dataset))
+
+batch_size = 10
+train_loader = DataLoader(transformed_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+def show_all_keypoints(image, predicted_key_pts, gt_pts=None):
+#   show image with predicted keypoints
+    plt.imshow(image, cmap='gray')
+    plt.scatter(predicted_key_pts[:, 0], predicted_key_pts[:, 1], s=20, marker='.', c='m')
+    if gt_pts is not None:
+        plt.scatter(gt_pts[:, 0], gt_pts[:, 1], s=20, marker='.', c='g')
+
+criterion = nn.SmoothL1Loss()
+optimizer = optim.Adam(net.parameters(), lr = 0.005)
+
+def train_net(n_epochs):
+    net.train()
+    for epoch in range(n_epochs):
+        running_loss = 0.0
+        for batch_i, data in enumerate(train_loader):
+            images = data['image']
+            key_pts = data['keypoints']
+            #flatten keypoints
+            key_pts = key_pts.view(key_pts.size(0), -1)
+            # convert variables to floats for regression loss
+            key_pts = key_pts.type(torch.FloatTensor)
+            images = images.type(torch.FloatTensor)
+
+            output_pts = net(images)
+            loss = criterion(output_pts, key_pts)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if batch_i % 10 == 9:    # print every 10 batches
+                print('Epoch: {}, Batch: {}, Avg. Loss: {}'.format(epoch + 1, batch_i+1, running_loss/10))
+                running_loss = 0.0
+    print('Finished Training')
+
+n_epochs = 15
+train_net(n_epochs)
